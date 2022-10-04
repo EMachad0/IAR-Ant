dd00a4dd4179d3a87d302f3e77ce295560587c41
EMachad0 <elitonmachadod200@gmail.com>
2022-10-04 14:10:05 -0300

    remove light flick

--- src/simulation/light.rs
+++ src/simulation/light.rs
@@ -4,6 +4,7 @@ use std::f32::consts::PI;
 
 use crate::consts::SUN_DISTANCE;
 use crate::simulation::info::SimulationInfo;
+use crate::timestep::FixedTimestepInfo;
 
 pub struct LightPlugin;
 
@@ -30,14 +31,26 @@ pub fn spawn_light(mut commands: Commands) {
                 shadows_enabled: true,
                 ..default()
             },
-            transform: Transform::from_xyz(0.0, SUN_DISTANCE, 0.0),
+            transform: Transform::from_translation(Vec3::Y * SUN_DISTANCE),
             ..default()
         })
         .insert(Sun);
 }
 
-pub fn sun_position_update(mut query: Query<&mut Transform, With<Sun>>, info: Res<SimulationInfo>) {
+pub fn sun_position_update(
+    mut query: Query<&mut Transform, With<Sun>>,
+    info: Res<SimulationInfo>,
+    timestep: Option<Res<FixedTimestepInfo>>,
+) {
     let mut transform = query.get_single_mut().expect("Unable to find Sun");
     let angle = info.update_count as f32 / 360.0 * (2. * PI);
-    transform.translation = vec3(angle.cos(), angle.sin(), 0.0) * SUN_DISTANCE;
+    let ups = match timestep {
+        None => 0.0,
+        Some(timestep) => 1.0 / timestep.step.as_secs_f64(),
+    };
+    transform.translation = if ups <= 5000.0 {
+        vec3(angle.cos(), angle.sin(), 0.0) * SUN_DISTANCE
+    } else {
+        Vec3::Y * SUN_DISTANCE
+    }
 }
